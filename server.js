02aa9fd8ff144281650274ad1d72ebb3bc3269da
lawcia <lawrencianjume@gmail.com>
2021-09-02 20:49:56 +0100

    object types

--- server.js
+++ server.js
@@ -4,21 +4,42 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
+type RandomDie {
+  numSides: Int!
+  rollOnce: Int!
+  roll(numRolls: Int!): [Int]
+}
+
 type Query {
-  rollDice(numDice: Int!, numSides: Int): [Int]
+  getDie(numSides: Int): RandomDie
 }
 `);
 
-// The root provides a resolver function for each API endpoint
-const root = {
-    rollDice: ({ numDice, numSides }) => {
-        var output = [];
-        for (var i = 0; i < numDice; i++) {
-            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+    constructor(numSides) {
+        this.numSides = numSides;
+    }
+
+    rollOnce() {
+        return 1 + Math.floor(Math.random() * this.numSides);
+    }
+
+    roll({ numRolls }) {
+        const output = [];
+        for (let i = 0; i < numRolls; i++) {
+            output.push(this.rollOnce());
         }
         return output;
     }
-};
+}
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    getDie: ({ numSides }) => {
+        return new RandomDie(numSides || 6);
+    }
+}
 
 const app = express();
 app.use('/graphql', graphqlHTTP({
