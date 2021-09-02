904b3d7bf5883e16d9b814fb1c7aeb9c9c72c685
lawcia <lawrencianjume@gmail.com>
2021-09-02 20:44:01 +0100

    pass arguments

--- server.js
+++ server.js
@@ -4,24 +4,20 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-  }
+type Query {
+  rollDice(numDice: Int!, numSides: Int): [Int]
+}
 `);
 
 // The root provides a resolver function for each API endpoint
 const root = {
-    quoteOfTheDay: () => {
-        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-    },
-    random: () => {
-        return Math.random();
-    },
-    rollThreeDice: () => {
-        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-    },
+    rollDice: ({ numDice, numSides }) => {
+        var output = [];
+        for (var i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+        }
+        return output;
+    }
 };
 
 const app = express();
