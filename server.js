9b15b88b83662efad29376d2f6c153ac063b70bb
lawcia <lawrencianjume@gmail.com>
2021-09-02 21:08:28 +0100

    authentication

--- server.js
+++ server.js
@@ -4,62 +4,21 @@ const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-input MessageInput {
-  content: String
-  author: String
-}
-
-type Message {
-  id: ID!
-  content: String
-  author: String
-}
-
 type Query {
-  getMessage(id: ID!): Message
-}
-
-type Mutation {
-  createMessage(input: MessageInput): Message
-  updateMessage(id: ID!, input: MessageInput): Message
+  ip: String
 }
 `);
 
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-    constructor(id, { content, author }) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
+const loggingMiddleware = (req, res, next) => {
+    console.log('ip:', req.ip);
+    next();
 }
-
-// Maps username to content
-const fakeDatabase = {};
 
 // The root provides a resolver function for each API endpoint
 const root = {
-    getMessage: ({ id }) => {
-        if (!fakeDatabase[id]) {
-            throw new Error('no message exists with id ' + id);
-        }
-        return new Message(id, fakeDatabase[id]);
-    },
-    createMessage: ({ input }) => {
-        // Create a random id for our "database".
-        const id = require('crypto').randomBytes(10).toString('hex');
-
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    },
-    updateMessage: ({ id, input }) => {
-        if (!fakeDatabase[id]) {
-            throw new Error('no message exists with id ' + id);
-        }
-        // This replaces all old data, but some apps might want partial update.
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    },
+    ip: function (args, request) {
+        return request.ip;
+    }
 };
 
 const app = express();
